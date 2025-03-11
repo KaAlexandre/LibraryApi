d1fe6970a906daa7bc91cf38a751e222ddf7ec84
Kauan Alexandre <atomicroyale334@gmail.com>
2025-03-11 18:31:29 -0300

    Adição de mais testes no AuthorRepositoryTests

--- src/test/java/io/github/cursodsousa/libraryapi/repository/AuthorRepositoryTests.java
+++ src/test/java/io/github/cursodsousa/libraryapi/repository/AuthorRepositoryTests.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -18,9 +19,9 @@ public class AuthorRepositoryTests {
     @Test
     public void saveTest() {
         Author author = new Author();
-        author.setName("Rodrigo Turini");
+        author.setName("Flavia Mantreal");
         author.setNationality("Brazilian");
-        author.setBirthDate(LocalDate.of(1980, 1, 1));
+        author.setBirthDate(LocalDate.of(2004, 11, 3));
 
         var authorSaved = authorRepository.save(author);
         System.out.printf("Author saved: %s", authorSaved);
@@ -41,5 +42,26 @@ public class AuthorRepositoryTests {
             System.out.println("Author updated:");
             System.out.println(foundAuthor);
         }
+    }
+
+    @Test
+    public void listTest() {
+        List<Author> authorsList = authorRepository.findAll();
+        authorsList.forEach(System.out::println);
+    }
+    @Test
+    public void countTest() {
+        System.out.println("Authors count: " + authorRepository.count());
+    }
+    @Test
+    public void  deleteByIdTest(){
+        var id = UUID.fromString("dc0c9375-46ba-45a6-b0ab-b8cf449e0c46");
+        authorRepository.deleteById(id);
+    }
+    @Test
+    public void deleteTest(){
+        var id = UUID.fromString("5d3530c6-0006-40b7-9770-9cf78a78082d");
+        var flavia = authorRepository.findById(id).get();
+        authorRepository.delete(flavia);
     }
 }
