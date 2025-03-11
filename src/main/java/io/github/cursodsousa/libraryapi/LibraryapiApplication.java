8c20d9e202c0e5959515771f71535890fe574eb2
Kauan Alexandre <atomicroyale334@gmail.com>
2025-03-11 17:47:24 -0300

    Utilização da classe para rodar um teste com o repositório author

--- src/main/java/io/github/cursodsousa/libraryapi/LibraryapiApplication.java
+++ src/main/java/io/github/cursodsousa/libraryapi/LibraryapiApplication.java
@@ -1,13 +1,29 @@
 package io.github.cursodsousa.libraryapi;
 
+import io.github.cursodsousa.libraryapi.model.Author;
+import io.github.cursodsousa.libraryapi.repository.AuthorRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.time.LocalDate;
 
 @SpringBootApplication
 public class LibraryapiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LibraryapiApplication.class, args);
+		var context = SpringApplication.run(LibraryapiApplication.class, args);
+		AuthorRepository authorRepository = context.getBean(AuthorRepository.class);
+
+		exampleSaveRegister(authorRepository);
 	}
 
+	public static void exampleSaveRegister(AuthorRepository authorRepository) {
+		Author author= new  Author();
+		author.setName("Rodrigo Turini");
+		author.setNationality("Brazilian");
+		author.setBirthDate(LocalDate.of(1980, 1, 1));
+
+		var authorSaved = authorRepository.save(author);
+		System.out.printf("Author saved: %s", authorSaved);
+	}
 }
