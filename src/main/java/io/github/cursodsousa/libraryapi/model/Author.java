956b39aa2e4b84bb167ffa0a366068f431adf5c3
Kauan Alexandre <atomicroyale334@gmail.com>
2025-03-11 16:17:23 -0300

    Mapeamento da entidade Author

---
+++ src/main/java/io/github/cursodsousa/libraryapi/model/Author.java
@@ -0,0 +1,30 @@
+package io.github.cursodsousa.libraryapi.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "author", schema = "public")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id")
+    private UUID id;
+
+    @Column(name = "name",length = 100, nullable = false)
+    private String name;
+
+    @Column(name = "birth_date", nullable = false)
+    private LocalDate birthDate;
+
+    @Column( name = "nationality", length = 50, nullable = false)
+    private String nationality;
+}
